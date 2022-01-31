e959584851cff3dacae12e68d6d1fb80970ef58f
tassun <tassun_oro@hotmail.com>
2022-01-31 17:26:45 +0700

    fws map
    add fws map

---
+++ fwg/plan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="plan" tilewidth="32" tileheight="32" tilecount="0" columns="100">
+ <image source="png/plan.png" width="3200" height="1235"/>
+</tileset>
