f8477f165b32d81f3901684de23a90a64b4ed5ca
tassun <tassun_oro@hotmail.com>
2022-02-01 10:00:58 +0700

    remove fwg map
    remove fwg map

--- fwg/dec.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="dec" tilewidth="32" tileheight="32" tilecount="728" columns="26">
- <image source="png/dec.png" width="832" height="896"/>
-</tileset>
--- fwg/wall.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.7.2" name="wall" tilewidth="32" tileheight="32" tilecount="775" columns="25">
- <image source="png/wall.png" width="800" height="992"/>
-</tileset>
