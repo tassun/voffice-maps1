26f6925b26995ec91c2ae5a801d51b7eca4221e7
tassun <tassun_oro@hotmail.com>
2022-02-03 14:03:55 +0700

    add script file
    add script file

---
+++ dist/script.js
@@ -0,0 +1,19 @@
+"use strict";
+/// <reference path="../node_modules/@workadventure/iframe-api-typings/iframe_api.d.ts" />
+let currentPopup = undefined;
+const today = new Date();
+const hh = today.getHours();
+const mm = today.getMinutes();
+const time = (((hh < 10) ? "0":"") + hh) + (((mm < 10) ? ":0" : ":") + mm);
+
+WA.room.onEnterZone('clock', () => {
+    currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
+});
+WA.room.onLeaveZone('clock', closePopUp);
+function closePopUp() {
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+//# sourceMappingURL=script.js.map
