dfab98c71fbf8c4c399ef358de203fa75a22c0a7
tassun <32536248+tassun@users.noreply.github.com>
2022-02-24 02:17:34 +0000

    Deploying to gh-pages from master @ e6b02f499d8e32c9a7e923ae424787047f5722d6 🚀

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,21 @@ const hh = today.getHours();
 const mm = today.getMinutes();
 const time = (((hh < 10) ? "0":"") + hh) + (((mm < 10) ? ":0" : ":") + mm);
 
-WA.room.onEnterLayer('clockZone').subscribe(() => {
-    currentPopup =  WA.ui.openPopup("clockPopup","It's " + time,[]);
-})
-
-WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
-
+const clockLists = [
+	{ "zone":"clock", "popup":"clockPopup" },
+	{ "zone":"clock1", "popup":"clock1Popup" },
+	{ "zone":"clock2", "popup":"clock2Popup" },	
+	{ "zone":"clock3", "popup":"clock3Popup" },	
+	{ "zone":"clock4", "popup":"clock4Popup" }	
+];
+for(var i=0,isz=clockLists.length;i<isz;i++) {
+	let ck = clockLists[i];
+	console.log(ck.zone+" = "+ck.popup);
+	WA.room.onEnterZone(ck.zone, () => {
+		currentPopup = WA.ui.openPopup(ck.popup, "It's " + time, []);
+	});
+	WA.room.onLeaveZone(ck.zone, closePopUp);	
+}
 function closePopUp(){
     if (currentPopup !== undefined) {
         currentPopup.close();
